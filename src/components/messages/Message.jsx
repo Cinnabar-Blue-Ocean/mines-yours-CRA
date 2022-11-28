dc2f48f6461b85776966b407a1f8ec016516fda0
Jonathan Liang <jonathanliang15@gmail.com>
2022-11-28 09:04:36 -0600

    Hello

--- src/components/messages/Message.jsx
+++ src/components/messages/Message.jsx
@@ -1,21 +1,26 @@
-import React from 'react';
+import React,{useRef, useEffect} from 'react';
 import {db, auth} from "../../firebase/index.js"
 import { ChatContext } from "./ChatContext.js"
 
 const Message = ({message}) => {
   const {currentUser} = auth;
   const {data} = ChatContext;
-  console.log(message)
+
+  const ref = useRef()
+
+  useEffect(() => {
+    ref.current?.scrollIntoView({behavior:"smooth"});
+  }, [message])
   return (
-    <div className="message owner">
-      {/* <div className="messageInfo">
+    <div ref={ref} className={`message ${message.senderId === currentUser.uid && "owner"}`}>
+      <div className="messageInfo">
         <img src="https://images.pexels.com/photos/13379797/pexels-photo-13379797.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=""/>
         <span>Just now</span>
       </div>
       <div className="messageContent">
-        <p>Hello</p>
-        <img src="https://images.pexels.com/photos/13379797/pexels-photo-13379797.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=""/>
-      </div> */}
+        <p>{message.text}</p>
+        {message.img && <img src={message.img} alt=""/>}
+      </div>
 
     </div>
   )
