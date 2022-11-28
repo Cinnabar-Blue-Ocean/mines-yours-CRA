c6d72970dbb9c7a4a842471cd23e18845df3f47e
mark clark <mark@idevforweb.com>
2022-11-27 21:07:15 -0700

    Update Message.jsx

--- src/components/messages/Message.jsx
+++ src/components/messages/Message.jsx
@@ -1,29 +1,18 @@
-import React,{useRef, useEffect} from 'react';
-import {db, auth} from "../../firebase/index.js"
-import { ChatContext } from "./ChatContext.js"
+import React from 'react';
 
-const Message = ({message}) => {
-  const {currentUser} = auth;
-  const {data} = ChatContext;
-
-  const ref = useRef()
-
-  useEffect(() => {
-    ref.current?.scrollIntoView({behavior:"smooth"});
-  }, [message])
+const Message = () => {
   return (
-    <div ref={ref} className={`message ${message.senderId === currentUser.uid && "owner"}`}>
+    <div className="message owner">
       <div className="messageInfo">
         <img src="https://images.pexels.com/photos/13379797/pexels-photo-13379797.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=""/>
         <span>Just now</span>
       </div>
       <div className="messageContent">
-        <p>{message.text}</p>
-        {message.img && <img src={message.img} alt=""/>}
+        <p>Hello</p>
+        <img src="https://images.pexels.com/photos/13379797/pexels-photo-13379797.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=""/>
       </div>
 
     </div>
   )
 }
-
 export default Message;
